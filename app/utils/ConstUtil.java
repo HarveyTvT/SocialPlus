b7e1c3fc0f82521a17b250e45f3569b28faec14c
HarveyTvT <mercershy58@gmail.com>
2015-09-07 15:41:05 +0800

    1. fix application.conf

--- app/utils/ConstUtil.java
+++ app/utils/ConstUtil.java
@@ -7,7 +7,13 @@ import com.typesafe.config.ConfigFactory;
  * Created by lizhuoli on 15/9/2.
  */
 public class ConstUtil {
-    public static final Config config = ConfigFactory.load();
+    static {
+        Config config = ConfigFactory.load();
+        weiboAppKey = config.getString("app.weibo.appkey");
+        weiboSecret = config.getString("app.weibo.secret");
+        weiboRedirectUrl = config.getString("app.weibo.redirectURL")
+    }
+
 
     public static final String UrlRegex =
             "^((http[s]?|ftp):\\/)?\\/?([^:\\/\\s]+)((\\/\\w+)*\\/)([\\w\\-\\.]+[^#?\\s]+)(.*)?(#[\\w\\-]+)?$";
@@ -15,7 +21,10 @@ public class ConstUtil {
     public static final String SHA1_SALT=
              "3faf0c40f18ecd2455671a4aaa88855ea4f85ad7";
 
-    public static final String weiboAuthUrl = String.format("https://api.weibo.com/oauth2/authorize?client_id=%s&redirect_uri=%s&response_type=code",weiboAppKey,weiboRedirectUrl);
+    public static final String weiboAuthUrl = String.format("https://api.weibo.com/oauth2/authorize?" +
+            "client_id=%s&redirect_uri=%s&" +
+            "response_type=code",
+            ConstUtil.weiboAppKey,ConstUtil.weiboRedirectUrl);
 
     public static final String renrenAuthUrl = "http://www.renren.com/callback";
 
@@ -28,9 +37,9 @@ public class ConstUtil {
     /*****************************************************************
       　　　　　　　　　微博相关的信息　　　　　　　　　　　
      *****************************************************************/
-    public static final String weiboAppKey = config.getString("app.weibo.appkey");
+    public static final String weiboAppKey ;
 
-    public static final String weiboSecret = config.getString("app.weibo.secret");
+    public static final String weiboSecret;
 
-    public static final String weiboRedirectUrl = config.getString("app.weibo.redirectURL");
+    public static final String weiboRedirectUrl ;
 }
